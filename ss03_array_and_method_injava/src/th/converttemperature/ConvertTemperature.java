c9fe72467cd050411e02958959818481f338872c
Tran Van Man <tranman123277@gmail.com>
2022-06-15 12:47:05 +0700

    3

---
+++ ss03_array_and_method_injava/src/th/converttemperature/ConvertTemperature.java
@@ -0,0 +1,4 @@
+package th.converttemperature;
+
+public class ConvertTemperature {
+}
--- ss03_array_and_method_injava/src/th/reversearray/ReverseArray.java
+++ ss03_array_and_method_injava/src/th/reversearray/ReverseArray.java
@@ -20,8 +20,17 @@ public class ReverseArray {
             i++;
         }
         System.out.printf("%-20%s", "Elements in array: ","");
+        for (int j=0;j<array.length;j++){
+            System.out.print(array[j] + "\t");
+        }
         for (int j=0;j<array.length/2;j++){
-            
+            int temp = array[j];
+            array[j] = array[size-1-j];
+            array[size-1-j] = temp;
+        }
+        System.out.printf("\n%-20s","Reverse array: ","");
+        for (int j=0;j<array.length;j++){
+            System.out.println(array[j] + "\t");
         }
     }
 }
