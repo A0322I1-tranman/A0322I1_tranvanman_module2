152971d2dea280a6697b64d1debfbd176cf6a71e
Tran Van Man <tranman123277@gmail.com>
2022-07-15 18:41:48 +0700

    bt1

--- ss12_JAVAcollectionFameWork/src/TH/Phanbiet/MainStu.java
+++ ss12_JAVAcollectionFameWork/src/TH/Phanbiet/MainStu.java
@@ -1,4 +1,6 @@
-package TH.HashMap;
+package TH.Phanbiet;
+
+import TH.Phanbiet.Student;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -7,21 +9,23 @@ import java.util.Set;
 
 public class MainStu {
     public static void main(String[] args) {
-     Student student1 = new Student("Đoàn Trần",25,"Đà Nẵng");
-     Student student2 = new Student("Đoàn Tống",29,"Hà Nội");
-     Student student3 = new Student("Đoàn Trân",27,"Huế");
+     Student student1 = new Student("Nam",20,"HN");
+     Student student2 = new Student("Hung",21,"HN");
+     Student student3 = new Student("Ha",22,"HN");
      Map<Integer,Student> studenMap = new HashMap<>();
      studenMap.put(1,student1);
      studenMap.put(2,student2);
      studenMap.put(3,student3);
+     studenMap.put(4,student1);
      for (Map.Entry<Integer,Student> students : studenMap.entrySet()){
          System.out.println(students.toString());
      }
-        System.out.println("---------");
+        System.out.println(".............Set");
         Set<Student> studentSet = new HashSet<Student>();
         studentSet.add(student1);
         studentSet.add(student2);
         studentSet.add(student3);
+        studentSet.add(student1);
         for (Student student : studentSet){
             System.out.println(student.toString());
         }
--- ss12_JAVAcollectionFameWork/src/TH/Phanbiet/Student.java
+++ ss12_JAVAcollectionFameWork/src/TH/Phanbiet/Student.java
@@ -1,4 +1,4 @@
-package TH.HashMap;
+package TH.Phanbiet;
 
 public class Student {
     private String name;
